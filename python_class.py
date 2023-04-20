994bd6e62a15571dd877402a63311e158d637af7
Jaiden Pauling <jpauling064@boyslatin.net>
2023-04-20 09:17:51 -0400

    python work

---
+++ python_class.py
@@ -0,0 +1,79 @@
+#my_pet_1 = 'pet'
+#my_pet_1_type = 'cat'
+#my_pet_1_noise = 'meow'
+#my_pet_1_full_name = 'Snuffles McGruff'
+
+#my_pet_2 = 'pet'
+#my_pet_2_type = 'cat'
+#my_pet_2_noise = 'meow'
+#my_pet_2_full_name = 'Snowpounce Flury'
+
+#my_pet_1= {
+    #'#Name' : 'Snuffles McGruff'
+#}
+
+#my_pet_2= {
+    #'Name' : 'Snowpounce Flury'
+#}
+
+#print(my_pet_1['Name'])
+#print(my_pet_2['Name'])
+# my_pets = [my_pet_1, my_pet_2]
+#for pet in my_pets:
+    #print(my_pet_1_full_name, my_pet_2_full_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Smoothies:
+    #def __init__(self, ingrediantA, ingrediantB, ingrediantC):
+        #self.ingrediantA = ingrediantA
+        #self.ingrediantB = ingrediantB
+        #self.ingrediantC = ingrediantC
+
+#order1 = Smoothies('mango', 'blueberry','Water')
+
+#print(order1.ingrediantA)
+#print(order1.ingrediantB)
+#print(order1.ingrediantC)
+
+
+class Best_Passer:
+    def __init__(self, passerA, height, ast):
+        self.passerA = passerA
+        self.height = height
+        self.apg = ast
+
+leader1 = Best_Passer('Magic Johnson', '6ft 9in', '11.2')
+
+print(leader1.passerA)
+print(leader1.height)
+print(leader1.apg + ' per game')
+
+leader2 = Best_Passer('John Stockton', '6ft 1in', '10.5')
+
+print(leader2.passerA)
+print(leader2.height)
+print(leader2.apg + ' per game')
+
+leader3 = Best_Passer('Chris Paul', '6ft in', '9.5')
+
+print(leader3.passerA)
+print(leader3.height)
+print(leader3.apg + ' per game')
+
